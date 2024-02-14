43673fb8c40ee2b57e0bf559e0bfab7f3083677d
Diego Ply <ply.dgo@gmail.com>
2024-02-14 10:06:50 +0100

    second commit

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,17 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Form\PostType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class PostController extends AbstractController
 {
-    #[Route('/', name: 'app_post')]
+    #[Route('/')]
     public function index(): Response
     {
         return $this->render('post/index.html.twig', [
            
         ]);
     }
+
+    #[Route('/post/new')]
+    public function create(Request $request): Response
+    {
+        $post = new Post();
+        $form = $this->createForm(PostType::class, $post);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            dump($post);
+        }
+        return $this->render('post/form.html.twig', [
+           "post_form" => $form->createView()
+        ]);
+    }
+
 }
