482a035762872284ef34d469b10d395a84275c07
Diego Ply <ply.dgo@gmail.com>
2024-02-16 10:48:27 +0100

    fin sessions9

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Form\LoginType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -10,15 +12,29 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
     {
         $error = $authenticationUtils->getLastAuthenticationError();
-
         $lastUserName = $authenticationUtils->getLastUsername();
 
+        $form = $this->createForm(LoginType::class);
+
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid())
+        {
+           
+        }
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUserName,
             'error' => $error,
+            "form" => $form->createView()
         ]);
+    }
+
+    #[Route('/logout', name: 'logout')]
+    public function logout()
+    {
+        
     }
 }
